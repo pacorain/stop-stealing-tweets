2d8d1667031a1439fb8ffaebb86aa34b264eab4c
Austin Paco DeArmond <hello@paco.audio>
2016-07-07 13:53:01 -0500

    Search for older tweets with exact text
    Detector now knows how to search for the tweet's text to see if
    there are matches. If there are, it will keep searching with
    max_id set to the lowest ID of the previous results until it finds
    the oldets tweet.
    
    There are some problems that still need to be fixed, including:
    
    * The text must be EXACT.
    * Line breaks aren't handled well.
    * Quotes in the original tweet cause some voodoo to happen.
    * Simple tweets (i.e. "I'm dying :joy::joy:") will have false
      positives.
    
    Still, this is a good start.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var TwitterWrapper = require('./lib/TwitterWrapper');
 var StolenTweetDetector = require('./lib/StolenTweetDetector');
+var StolenStatus = require('./lib/StolenStatus');
 var auth = require('./config/auth.json').twitter;
 var accounts = require('./config/accounts.json');
-
 
 var twitter = new TwitterWrapper(require('./config/auth.json').twitter);
 var detector = new StolenTweetDetector(twitter);
@@ -25,16 +25,24 @@ function setUpStream() {
 
 function checkIfStolen(tweet) {
   console.log(tweet.user.screen_name + " tweeted, testing if stolen.");
-  if (detector.mediaIsStolen(tweet)) {
-    console.log("test");
-    detector.getMediaOwner(tweet).then(function (user) { 
-      console.log("Media was stolen from @" + user.screen_name + ".");
+  detector.processMedia(tweet).then(function(mediaIsStolen) {
+    if (mediaIsStolen) return StolenStatus.RELINKED_MEDIA;
+    else return detector.processText(tweet).then(function (textIsStolen) {
+      if (textIsStolen) return StolenStatus.COPIED_TEXT;
+      else return StolenStatus.NONE;
     });
-  } else if (tweetTextOriginatedElsewhere(tweet)) {
-    // TODO
-  } else {
-    console.log("Could not detect that the tweet was stolen.");
-  }
+  }).then(function (status) {
+    if (status === StolenStatus.NONE)
+      console.log("Could not detect that the tweet was stolen.");
+    else if (status === StolenStatus.RELINKED_MEDIA)
+      detector.getMediaOrigin(tweet).then(function (origin) { 
+        console.log("Media was stolen from @" + origin.user.screen_name + ".");
+      });
+    else if (status === StolenStatus.COPIED_TEXT)
+      detector.getOrigin(tweet).then(function (origin) {
+        console.log("Tweet was stolen from @" + origin.user.screen_name + ".");
+      });
+  }).fail(console.error);
 }
 
 function tweetTextOriginatedElsewhere(tweet) {
