7978c1380bcc0886608f8bec7c608a8417ac9163
Austin Paco DeArmond <hello@paco.audio>
2016-07-07 10:48:26 -0500

    Watch for stolen media (sometimes, see #1)
    This commit adds a StolenTweetDetector that currently can detect
    if media in a tweet originiated in another Tweet from another
    account IF the source information is presented in the stream.
    Unfortunatley, this is not always the case, so issue #1 is not
    completely resolved.
    
    TwitterWrapper also has some helper methods.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var TwitterWrapper = require('./lib/TwitterWrapper');
+var StolenTweetDetector = require('./lib/StolenTweetDetector');
 var auth = require('./config/auth.json').twitter;
 var accounts = require('./config/accounts.json');
 
-var twitter = new TwitterWrapper(auth);
+
+var twitter = new TwitterWrapper(require('./config/auth.json').twitter);
+var detector = new StolenTweetDetector(twitter);
 var stream;
 
 setUpStream();
@@ -14,12 +17,26 @@ function setUpStream() {
     stream.on('tweet', function(tweet) {
       checkIfStolen(tweet);
     });
-    stream.on('error', function(err) {
-      console.err(err);
+    stream.on('error', function(error) {
+      console.error(error);
     });
-  }).then(function(error) {console.log("test" + error)});
+  }).fail(console.log);
 }
 
 function checkIfStolen(tweet) {
-  console.log(tweet.user.screen_name + " tweeted: " + tweet.text);
+  console.log(tweet.user.screen_name + " tweeted, testing if stolen.");
+  if (detector.mediaIsStolen(tweet)) {
+    console.log("test");
+    detector.getMediaOwner(tweet).then(function (user) { 
+      console.log("Media was stolen from @" + user.screen_name + ".");
+    });
+  } else if (tweetTextOriginatedElsewhere(tweet)) {
+    // TODO
+  } else {
+    console.log("Could not detect that the tweet was stolen.");
+  }
+}
+
+function tweetTextOriginatedElsewhere(tweet) {
+  return false;
 }
