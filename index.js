74bc321da4db6d0be88daccc040810bce7a5398b
Austin Paco DeArmond <hello@paco.audio>
2016-07-09 12:34:46 -0500

    Create new StolenTweetDetector for each new tweet
    The flow made it impossible to add new functionality, including
    confidence that the tweet was stolen. Having an object that has
    data about the origin tweet makes since.
    
    Also start working on Natural Languate processing (NL).

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var auth = require('./config/auth.json').twitter;
 var accounts = require('./config/accounts.json');
 
 var twitter = new TwitterWrapper(require('./config/auth.json').twitter);
-var detector = new StolenTweetDetector(twitter);
 var stream;
 
 setUpStream();
 
 function setUpStream() {
-  console.log("INDEX: Setting up stream.")
+  console.log("INDEX: Setting up stream.");
   twitter.watchUsers(accounts).progress(function (twitterStream) {
     stream = twitterStream;
     stream.on('tweet', function(tweet) {
@@ -25,26 +24,30 @@ function setUpStream() {
 
 function checkIfStolen(tweet) {
   console.log(tweet.user.screen_name + " tweeted, testing if stolen.");
-  detector.processMedia(tweet).then(function(mediaIsStolen) {
-    if (mediaIsStolen) return StolenStatus.RELINKED_MEDIA;
-    else return detector.processText(tweet).then(function (textIsStolen) {
-      if (textIsStolen) return StolenStatus.COPIED_TEXT;
-      else return StolenStatus.NONE;
-    });
-  }).then(function (status) {
-    if (status === StolenStatus.NONE)
-      console.log("Could not detect that the tweet was stolen.");
-    else if (status === StolenStatus.RELINKED_MEDIA)
-      detector.getMediaOrigin(tweet).then(function (origin) { 
-        console.log("Media was stolen from @" + origin.user.screen_name + ".");
-      });
-    else if (status === StolenStatus.COPIED_TEXT)
-      detector.getOrigin(tweet).then(function (origin) {
-        console.log("Tweet was stolen from @" + origin.user.screen_name + ".");
-      });
+  var detector = new StolenTweetDetector(tweet, twitter);
+  detector.findOrigin().then(function () {
+    process(detector);
   }).fail(console.error);
 }
 
-function tweetTextOriginatedElsewhere(tweet) {
-  return false;
+function process(detector) {
+  if (detector.getStatus() === StolenStatus.NONE) {
+    console.log("Could not detect that the tweet was stolen.");
+  } else if (!detector.isConfident()) {
+    console.log("The tweet might be stolen, but cannot be confidently marked.")
+  } else if (detector.getStatus() === StolenStatus.RELINKED_MEDIA) {
+    handleStolenMedia(detector);
+  } else if (detector.getStatus() === StolenStatus.COPIED_TEXT) {
+    handleStolenText(detector);
+  }
+}
+
+function handleStolenMedia(detector) {
+  var origin = detector.getOrigin();
+  console.log("Media was stolen from @" + origin.user.screen_name + ".");
+}
+
+function handleStolenText(detector) {
+  var origin = detector.getOrigin();
+  console.log("Tweet text was stolen from @" + origin.user.screen_name + ".");
 }
